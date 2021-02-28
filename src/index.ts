c261cae543b6d1f26d33a7cc8509590f7de48cd6
charliejuc <charliejuc@gmail.com>
2021-02-28 20:30:42 +0100

    Adds CurryV1

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,24 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment,@typescript-eslint/no-explicit-any */
-type Head<T extends unknown[]> = T extends [infer E, ...unknown[]] ? E : never
 type Tail<T extends unknown[]> = T extends [unknown, ...infer ET] ? ET : []
-type HasTail<T extends unknown[]> = T extends [] | [unknown] ? false : true
+type Prepend<E, P extends unknown[]> = [E, ...P] extends [...infer EP] ? EP : P
+type Drop<N extends number, P extends unknown[], C extends unknown[] = []> = {
+    continueDropping: Drop<N, Tail<P>, Prepend<unknown, C>>
+    return: P
+}[C['length'] extends N ? 'return' : 'continueDropping']
+type Cast<T, C> = T extends C ? T : C
 
-type CurryV0<P extends unknown[], R> = (
-    argN: Head<P>
-) => HasTail<P> extends true ? CurryV0<Tail<P>, R> : R
+type CurryV1<P extends unknown[], R> = <T extends unknown[]>(
+    ...args: Cast<T, Partial<P>>
+) => Drop<T['length'], P> extends [unknown, ...unknown[]]
+    ? // @ts-ignore
+      CurryV1<Cast<Drop<T['length'], P>, unknown[]>, R>
+    : R
 
 const curry = <F extends (...args: any[]) => any, P extends Parameters<F>, R extends ReturnType<F>>(
     fn: F
-): CurryV0<P, R> => {
-    const curryStepFactory = (prevArgs: any[]): any => (argN: Head<P>): any => {
-        const passedArgs = [...prevArgs, argN]
+): CurryV1<P, R> => {
+    const curryStepFactory = (prevArgs: any[]): any => (...args: any[]): any => {
+        const passedArgs = [...prevArgs, ...args]
 
         if (passedArgs.length < fn.length) {
             return curryStepFactory(passedArgs)
@@ -28,9 +35,12 @@ const getMessage = (base: string, amount: number, tail: string): string =>
 
 const getMessageCurried = curry(getMessage)
 
+// getMessageCurried('sdfsad')(2)
+
+console.log(getMessageCurried('1 We have', 2, 'apples'))
 console.log(getMessageCurried('1 We have'))
 console.log(getMessageCurried('2 We have')(2))
 console.log(getMessageCurried('3 We have')(3)('apples'))
-// console.log(getMessageCurried('We have', 2)('apples'))
-// console.log(getMessageCurried('We have')(2, 'apples'))
-// console.log(getMessageCurried('We have', 2, 'apples'))
+console.log(getMessageCurried('4 We have', 2)('apples'))
+console.log(getMessageCurried('5 We have')(8, 'apples'))
+console.log(getMessageCurried('6 We have', 2, 'apples'))
